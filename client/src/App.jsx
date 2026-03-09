7636510ebd8e745a637a2e53ab7293f09cf72bcc
poison2912 <susan.dev291@gmail.com>
2026-03-09 18:04:40 +0545

    initialize

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+ 
+
+  return (
+    <>
+    <h1 className="text-2xl text-red-600 text-center mt-52 font-bold">Test</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module github.com/poison291/riff
+
+go 1.25.6
